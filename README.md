ecc80a5ee0758930385c5ad4a6bf1619e0cc79cf
Andrea Giammarchi <a.giammarchi@eyeo.com>
2019-01-10 18:57:50 +0100

    added all the info

---
+++ README.md
@@ -0,0 +1,23 @@
+# hyperhtml-wire
+
+[![Build Status](https://travis-ci.com/WebReflection/hyperhtml-wire.svg?branch=master)](https://travis-ci.com/WebReflection/hyperhtml-wire) [![Coverage Status](https://coveralls.io/repos/github/WebReflection/hyperhtml-wire/badge.svg?branch=master)](https://coveralls.io/github/WebReflection/hyperhtml-wire?branch=master) ![WebReflection status](https://offline.report/status/webreflection.svg)
+
+The persistent fragment like class used in [hyperHTML](https://github.com/WebReflection/hyperHTML).
+
+This is particularly useful with [domdiff](https://github.com/WebReflection/domdiff) so it's been extracted from _hyperHTML_ itself..
+
+```js
+import Wire from 'hyperhtml-wire';
+
+const wire = new Wire(document.body.querySelectorAll('*'));
+
+wire.first; // first body element
+wire.last;  // last body element
+
+wire.remove(true);  // remove all but first node (domdiff friendly)
+wire.remove(false); // remove all nodes
+
+wire.valueOf(true); // force nodes to be re-appended to a fragment
+wire.remove(false); // returns fragment with nodes only if not appended
+
+```
--- index.js
+++ index.js
@@ -3,18 +3,6 @@ var Wire = (function (slice, proto) {
 
   proto = Wire.prototype;
 
-  proto.valueOf = function (forceAppend) {
-    var frag = this.f;
-    var noFrag = frag == null;
-    if (noFrag)
-      frag = (this.f = this.d.createDocumentFragment());
-    if (noFrag || forceAppend) {
-      for (var n = this.n, i = 0, l = n.length; i < l; i++)
-        frag.appendChild(n[i]);
-    }
-    return frag;
-  };
-
   proto.remove = function (keepFirst) {
     var childNodes = this.n;
     var first = this.first;
@@ -31,12 +19,25 @@ var Wire = (function (slice, proto) {
     return first;
   };
 
+  proto.valueOf = function (forceAppend) {
+    var frag = this.f;
+    var noFrag = frag == null;
+    if (noFrag)
+      frag = (this.f = this.d.createDocumentFragment());
+    if (noFrag || forceAppend) {
+      for (var n = this.n, i = 0, l = n.length; i < l; i++)
+        frag.appendChild(n[i]);
+    }
+    return frag;
+  };
+
   return Wire;
 
   function Wire(childNodes) {
     var nodes = (this.n = slice.call(childNodes, 0));
-    this.d = (this.first = nodes[0]).ownerDocument;
+    this.first = nodes[0];
     this.last = nodes[nodes.length - 1];
+    this.d = nodes[0].ownerDocument;
     this.f = null;
   }
 
